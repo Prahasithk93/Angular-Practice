04cc71313887bd2eac7def280668c8e611532634
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-12 15:49:25 +0530

    stylebinding

--- check/src/app/app.component.ts
+++ check/src/app/app.component.ts
@@ -4,35 +4,21 @@ import { reduce } from 'rxjs';
 @Component({
   selector: 'app-root',
   template:`
-  <h2 class="text-color">welcome {{name}}</h2>
-  <input bind-disabled="isDisabled" [id]="ID" type="text" value="prahasith">
-  <h2 [class]="color">welcome {{name}}</h2>
-  <h2 [class.text-color]="hasError">welcome {{name}}</h2>
-  <h2 [ngClass]="Classes">welcome {{name}}</h2>
+  <h2 [style.color]="'red'">hello {{name}}</h2>
+  <h2 [style.color]="isError?'green':'blue'">hello {{name}}</h2>
+  <h2 [style.color]="textColor">hello {{name}}</h2>
+  <h2 [ngStyle]="titleStyle">hello {{name}}</h2>
   ` ,
   styles: [`
-    .text-success{
-      color: red;
-    }
-    .text-failure{
-      font-size:50px;
-    }
-    .text-tie{
-      font-style:italic;
-    }
+
   `]
 })
 export class AppComponent {
-  title = 'Hello-world';
   public name="prahasith";
-  public color="text-color";
-  public ID="testid";
-  public isDisabled=true;
-  public hasError=false;
-  public Classes={
-    "text-success": !this.hasError,
-    "text-failure": this.hasError,
-    "text-tie":this.isDisabled 
+  public isError=false;
+  public textColor="orange";
+  public titleStyle={
+      color: "blue",
+      fontStyle:"italic"
   }
-  
 }
