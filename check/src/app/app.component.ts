46f0e9722d00e614020d4eba97dd34a23b63382c
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-12 17:54:53 +0530

    twoWayBinding

--- check/src/app/app.component.ts
+++ check/src/app/app.component.ts
@@ -1,26 +1,23 @@
 import { Component} from '@angular/core';
 import { reduce } from 'rxjs';
 
+
 @Component({
   selector: 'app-root',
   template:`
-  <h2 >hello {{name}}</h2>
-  <input #input type="text">
-  <button (click)="message(input.value)">click me</button>
-  {{input.value}}
+  <input [(ngModel)]="name" type="text">
+  {{name}}
   ` ,
   styles: [`
 
   `]
 })
 export class AppComponent {
-  public name="prahasith";
+  public name="";
   
   constructor() { }
 
   ngOnInit(): void {
   }
-  message(value:any){
-    console.log(value);
-  }
+ 
 }
--- check/src/app/app.module.ts
+++ check/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularComponent } from './angular/angular.component';
 import { Test1Component } from './test1/test1.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,8 @@ import { Test1Component } from './test1/test1.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
