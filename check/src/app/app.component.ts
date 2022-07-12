d7e434e6a2c0f623db60fc27322c26aeec61eba8
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-12 15:16:37 +0530

    classbinding

--- check/src/app/app.component.ts
+++ check/src/app/app.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template:`
-  <h2>welcome {{name}}</h2>
+  <h2 class="text-color">welcome {{name}}</h2>
   <input bind-disabled="isDisabled" [id]="ID" type="text" value="prahasith">
+  <h2 [class]="color">welcome {{name}}</h2>
+  <h2 [class.text-color]="hasError">welcome {{name}}</h2>
+  <h2 [ngClass]="Classes">welcome {{name}}</h2>
   ` ,
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .text-success{
+      color: red;
+    }
+    .text-failure{
+      font-size:50px;
+    }
+    .text-tie{
+      font-style:italic;
+    }
+  `]
 })
 export class AppComponent {
   title = 'Hello-world';
-  public name="prahasith"
+  public name="prahasith";
+  public color="text-color";
   public ID="testid";
   public isDisabled=true;
+  public hasError=false;
+  public Classes={
+    "text-success": !this.hasError,
+    "text-failure": this.hasError,
+    "text-tie":this.isDisabled 
+  }
+  
 }
