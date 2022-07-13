ac16e27fcd7c3046c01ec58f1b3ca4dc92ab65ae
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-13 15:20:01 +0530

    component interaction parent to child

--- check/src/app/angular/angular.component.ts
+++ check/src/app/angular/angular.component.ts
@@ -1,34 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-angular',
   template: `
-  <h1>for index </h1>
-    <div *ngFor="let car of cars; index as i">
-        <p>{{i}} {{car}}</p>
-    </div>
-    <h1>for first index </h1>
-    <div *ngFor="let car of cars; first as f">
-        <p>{{f}} {{car}}</p>
-    </div>
-    <h1>for last index </h1>
-    <div *ngFor="let car of cars; last as l">
-        <p>{{l}} {{car}}</p>
-    </div>
-    <h1>for even index </h1>
-    <div *ngFor="let car of cars; even as e">
-        <p>{{e}} {{car}}</p>
-    </div>
-    <h1>for odd index </h1>
-    <div *ngFor="let car of cars; odd as o">
-        <p>{{o}} {{car}}</p>
-    </div>
+    <h2>{{"good morning "+name}}</h2>
   `,
   styleUrls: ['./angular.component.css']
 })
 export class AngularComponent implements OnInit {
 
-  public cars = ["maruti", "volvo", "audi", "tata"];
+  @Input('parentData') public name: any;
   constructor() { }
 
   ngOnInit(): void {
--- check/src/app/app.component.ts
+++ check/src/app/app.component.ts
@@ -1,29 +1,27 @@
-// import { Component} from '@angular/core';
-// import { reduce } from 'rxjs';
+import { Component} from '@angular/core';
+import { reduce } from 'rxjs';
 
 
-// @Component({
-//   selector: 'app-root',
-//   template:`<ng-template><div>Hello</div></ng-template>` ,
-//   styles: [`
-
-//   `]
-// })
-// export class AppComponent {
-//   public name=true
-  
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
- 
-// }
-import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public name="prahasith";
   title = 'project';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+ 
 }
+// import { Component } from '@angular/core';
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'project';
+// }
