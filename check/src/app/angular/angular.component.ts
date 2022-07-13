fe4a9232be45376504de4fb10f3d9650e28edf5d
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-13 14:20:52 +0530

    ngFor

--- check/src/app/angular/angular.component.ts
+++ check/src/app/angular/angular.component.ts
@@ -3,17 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-angular',
   template: `
-    <div [ngSwitch]="number">
-      <div *ngSwitchCase="'1'">number selected one</div>
-      <div *ngSwitchCase="'2'">number selected two</div>
-      <div *ngSwitchCase="'3'">number selected three</div>
-      <div *ngSwitchDefault>no number selected</div>
+    <div *ngFor="let car of cars">
+        <p>{{car}}</p>
     </div>
   `,
   styleUrls: ['./angular.component.css']
 })
 export class AngularComponent implements OnInit {
-public number=2;
+
+    public cars=["maruti","volvo","audi","tata"];
   constructor() { }
 
   ngOnInit(): void {
