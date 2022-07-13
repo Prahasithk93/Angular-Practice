7783f4cd9f6a4aedfaae49c6eec668c5ff47497d
prahasithk93 <prahasith.kuncham@prasthana.com>
2022-07-13 10:51:19 +0530

    ngif

--- check/src/app/angular/angular.component.ts
+++ check/src/app/angular/angular.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-angular',
-  templateUrl: './angular.component.html',
+  template: `<h2 *ngIf="name;else elseError">hello  prahasith</h2>
+  <ng-template #elseError><h2>good morning</h2></ng-template>
+  <div *ngIf="name; then thenB; else elseB"; ></div>
+  <ng-template #thenB>
+    <h2>code Evolution</h2>
+  </ng-template>
+  <ng-template #elseB>
+    <h2>Hidden</h2>
+  </ng-template>
+  `,
   styleUrls: ['./angular.component.css']
 })
 export class AngularComponent implements OnInit {
-
+public name=false
   constructor() { }
 
   ngOnInit(): void {
--- check/src/app/app.component.ts
+++ check/src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component} from '@angular/core';
-import { reduce } from 'rxjs';
+// import { Component} from '@angular/core';
+// import { reduce } from 'rxjs';
 
 
+// @Component({
+//   selector: 'app-root',
+//   template:`<ng-template><div>Hello</div></ng-template>` ,
+//   styles: [`
+
+//   `]
+// })
+// export class AppComponent {
+//   public name=true
+  
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+ 
+// }
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  template:`
-  <input [(ngModel)]="name" type="text">
-  {{name}}
-  ` ,
-  styles: [`
-
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public name="";
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
- 
+  title = 'project';
 }
